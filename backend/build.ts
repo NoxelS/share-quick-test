06b7182e5d0900365d095e4fd272d7dba0b2ed05
Noel Schwabenland <noel@schwabenland.ch>
2020-10-16 12:29:10 +0200

    refactor

--- backend/build.ts
+++ backend/build.ts
@@ -1,13 +1,10 @@
 import fs from 'fs-extra';
-import Logger from 'jet-logger';
 
+
+/** This will be executed before the main build starts. */
 
 try {
-    // Remove current build
     fs.removeSync('./dist/');
-    // Copy front-end files
-    fs.copySync('./src/public', './dist/public');
-    fs.copySync('./src/views', './dist/views');
 } catch (err) {
-    Logger.Err(err);
+    console.error(err);
 }
--- backend/src/Server.ts
+++ backend/src/Server.ts
@@ -1,45 +1,42 @@
 import cookieParser from 'cookie-parser';
-import express, { NextFunction, Request, Response } from 'express';
+import cors from 'cors';
+import { config } from 'dotenv';
+import express, { json, NextFunction, Request, Response, urlencoded } from 'express';
 import 'express-async-errors';
 import helmet from 'helmet';
-import StatusCodes from 'http-status-codes';
 import morgan from 'morgan';
-import path from 'path';
 
-import BaseRouter from './routes';
+import ApiRouter from './routes';
 
 
+// Load env
+config();
+
+// Set CORS options
+const corsOptions: cors.CorsOptions = process.env.NODE_ENV === 'production' ? { origin: process.env.CORS_ORIGIN } : { origin: '*' };
+
+// Init express
 const app = express();
-const { BAD_REQUEST } = StatusCodes;
-
-
 
 app.use(express.json());
-app.use(express.urlencoded({extended: true}));
+app.use(express.urlencoded({ extended: true }));
 app.use(cookieParser());
-
-// Show routes called in console during development
-if (process.env.NODE_ENV === 'development') {
+app.use(cors(corsOptions));
+if (process.env.NODE_ENV === 'production') {
+    // Security
+    app.use(helmet());
+} else {
+    // Show routes called in console during development
     app.use(morgan('dev'));
 }
 
-// Security
-if (process.env.NODE_ENV === 'production') {
-    app.use(helmet());
-}
-
 // Add APIs
-app.use('/api', BaseRouter);
+app.use('/api', ApiRouter);
 
 // Print API errors
-// eslint-disable-next-line @typescript-eslint/no-unused-vars
 app.use((err: Error, req: Request, res: Response, next: NextFunction) => {
-    console.error(err, true);
-    return res.status(BAD_REQUEST).json({
-        error: err.message,
-    });
+    return res.json((err.message));
 });
-
 
 // Export express instance
 export default app;
